9963d37904cda08bb4d23648db94d167d25ab1e0
Gonçalo <70868465+Saleca@users.noreply.github.com>
2024-09-29 17:56:23 +0100

    Delete settings.js

--- settings.js
+++
@@ -1,6 +0,0 @@
-fetch('settings.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('settings').innerHTML = data;
-            })
-            .catch(error => console.error('Error fetching settings:', error));
