8ba081040691b252724a1d3d07170960a29b64f2
Gonçalo <70868465+Saleca@users.noreply.github.com>
2025-04-21 18:59:52 +0100

    mod snake page

---
+++ js/modal-manager.js
@@ -0,0 +1,54 @@
+const overlay = document.getElementById("overlay");
+const modalButtons = document.querySelectorAll(".modal-button");
+const closeModalButtons = document.querySelectorAll(".close-modal");
+
+function stopClickPropagation(event) {
+    event.stopPropagation();
+}
+
+var currentModal;
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (!modal) {
+        return;
+    }
+    currentModal = modal;
+    overlay.style.display = "block";
+    modal.style.display = "block";
+    modal.addEventListener("click", stopClickPropagation);
+}
+
+function closeModal() {
+    overlay.style.display = "none";
+    currentModal.style.display = "none";
+    currentModal.removeEventListener("click", stopClickPropagation);
+}
+
+function initialize() {
+    //associate modal with button
+    modalButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const targetModalId = this.dataset.modalTarget;
+            openModal(targetModalId);
+        });
+    });
+
+    //add event listeners to close modals
+    closeModalButtons.forEach((button) => {
+        button.addEventListener("click", closeModal);
+    });
+
+    overlay.addEventListener("click", function (event) {
+        closeModal();
+    });
+
+    const linkElement = document.createElement('link');
+
+    linkElement.rel = 'stylesheet';
+    linkElement.type = 'text/css';
+    linkElement.href = '../css/modal.css';
+
+    document.head.appendChild(linkElement);
+}
+
+initialize();
